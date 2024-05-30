d58acf200e9fd0966b5e8370d10b8b70ff655412
Thiago César <66066163+thiagoprogramando@users.noreply.github.com>
2024-05-30 02:08:59 -0300

    Add Logo MaisMed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,7 @@
 
         <title>Acesso ao {{ env('APP_NAME') }}</title>
 
-        <link href="{{ asset('dashboard/img/favicon.png') }}" rel="icon">
-        <link href="{{ asset('dashboard/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+        <link href="{{ asset('dashboard/img/logo.png') }}" rel="icon">
 
         <link href="https://fonts.gstatic.com" rel="preconnect">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -63,7 +62,7 @@
                                 </div>
 
                                 <div class="credits">
-                                    Desenvolvido por <a href="https://ifuture.cloud/">IFUTURE</a>
+                                    Desenvolvido por <a href="https://ifuture.cloud/">ifuture.cloud</a>
                                 </div>
 
                             </div>
